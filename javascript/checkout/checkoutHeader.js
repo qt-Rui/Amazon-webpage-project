239073f73e4c5248de388808ca0f185defb1bea1
qt-Rui <christopherd1229@gmail.com>
2025-05-02 23:46:02 -0400

    Update checkoutHeader.js
    From amazon.html to index.html

--- javascript/checkout/checkoutHeader.js
+++ javascript/checkout/checkoutHeader.js
@@ -10,7 +10,7 @@ export function renderCheckoutHeader() {
     const checkoutHeaderHTML = `
         <div class="header-content">
         <div class="checkout-header-left-section">
-          <a href="amazon.html">
+          <a href="index.html">
             <img class="amazon-logo" src="images/amazon-logo.png">
             <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
           </a>
@@ -18,7 +18,7 @@ export function renderCheckoutHeader() {
 
         <div class="checkout-header-middle-section">
           Checkout (<a class="return-to-home-link js-return-to-home-link"
-            href="amazon.html">${cartQuantity} items</a>)
+            href="index.html">${cartQuantity} items</a>)
         </div>
 
         <div class="checkout-header-right-section">
